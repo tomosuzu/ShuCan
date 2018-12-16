6bb7558361525fef590efe82e89cfe4d1a49c9cd
tomosuzu <tomoya_su40@hotmail.com>
2018-12-16 10:41:48 +0900

    [update] add Input field

--- src/components/App.test.tsx
+++ src/components/App.test.tsx
@@ -4,10 +4,11 @@ import { shallow } from 'enzyme';
 import { Hello } from './App';
 
 describe('index', () => {
-  test('show title', () => {
+  test('show title and description', () => {
     const wrapper = shallow(<Hello compiler="TypeScript" framework="React"/>);
 
     expect(wrapper.find('h1').length).toBe(1);
+    expect(wrapper.find('p').length).toBe(1);
+    expect(wrapper.find('input').length).toBe(1);
   });
-
 });
--- src/components/App.tsx
+++ src/components/App.tsx
@@ -4,6 +4,12 @@ export interface HelloProps { compiler: string; framework: string; }
 
 export class Hello extends React.Component<HelloProps, {}> {
   render() {
-    return <h1>Hello from {this.props.compiler} and {this.props.framework}!</h1>;
+    return(
+      <div>
+        <h1>Welcome to ShuCan</h1>
+        <p>Hello from {this.props.compiler} and {this.props.framework}!</p>
+        <input type="text"/>
+      </div>
+    );
   }
 }
