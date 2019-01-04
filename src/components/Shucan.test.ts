23fb01121c22cdd34c1f06364ba0b2c424f626a0
tomosuzu <tomoya_su40@hotmail.com>
2019-01-04 11:51:39 +0900

    [clean] from it to test

--- src/components/Shucan.test.ts
+++ src/components/Shucan.test.ts
@@ -5,7 +5,7 @@ describe('Shucan', () => {
   beforeEach(() => {
     state = { shucan: [] };
   });
-  it('add Shucan\'s title & repeat type', () => {
+  test('add Shucan\'s title & repeat type', () => {
     Shucan(state, addShucan('title1', RepeatType.day));
     const result = Shucan(state, addShucan('title2', RepeatType.week));
 
@@ -15,13 +15,13 @@ describe('Shucan', () => {
     expect(result.shucan[1].repeat).toBe(RepeatType.week);
   });
 
-  it('add Shucan\'s no option' , () => {
+  test('add Shucan\'s no option' , () => {
     const result = Shucan(state, addShucan('title2', RepeatType.week));
 
     expect(result.shucan[0].endDate).toBe(undefined);
   });
 
-  it('add Shucan\'s endDate' , () => {
+  test('add Shucan\'s endDate' , () => {
     const options = {
       endDate: '20180103',
     };
